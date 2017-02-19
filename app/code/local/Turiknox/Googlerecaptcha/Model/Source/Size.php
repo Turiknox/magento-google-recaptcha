4db7056000db7e49dc52f74937b61c2790f3c0b2
Turiknox <parsonsss91@hotmail.com>
2017-02-19 14:29:21 +0000

    Correcting licence URL

--- app/code/local/Turiknox/Googlerecaptcha/Model/Source/Size.php
+++ app/code/local/Turiknox/Googlerecaptcha/Model/Source/Size.php
@@ -5,7 +5,7 @@
  * @category   Turiknox
  * @package    Turiknox_Googlerecaptcha
  * @copyright  Copyright (c) 2017 Turiknox
- * @license    https://github.com/turiknox/magento-google-recaptcha/LICENSE.md
+ * @license    https://github.com/Turiknox/magento-google-recaptcha/blob/master/LICENSE.md
  * @version    1.0.0
  */
 class Turiknox_Googlerecaptcha_Model_Source_Size
